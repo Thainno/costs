cff69602103ba31f4ff90d15971ffe79f97c82ae
Tainu <thainnosv@gmail.com>
2024-04-05 10:59:26 -0300

    components

---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase() {
+  return (
+    <div>
+      <p>Este é um componentecom uma frase!</p>
+    </div>
+  );
+}
+
+export default Frase;
---
+++ src/components/HelloWord.js
@@ -0,0 +1,12 @@
+import Frase from "./Frase";
+
+function HelloWord() {
+  return (
+    <div>
+      <h1>Primeiro componente</h1>
+      <Frase />
+    </div>
+  );
+}
+
+export default HelloWord;
