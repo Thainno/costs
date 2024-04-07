757574621279f8d60c57d84e36c4dfcab955eac5
Tainu <thainnosv@gmail.com>
2024-04-07 14:30:22 -0300

    react icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,24 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+import styles from "./Footer.module.css";
+
 function Footer() {
-    return(
-        <p>Rodapé</p>
-    )
+  return (
+    <footer>
+      <ul className={styles.social_list}>
+        <li>
+          <FaFacebook className={styles.icon_facebook} />
+        </li>
+        <li>
+          <FaInstagram className={styles.icon_instagram} />
+        </li>
+        <li>
+          <FaLinkedin className={styles.icon_linkedin} />
+        </li>
+      </ul>
+      <p>Meu rodapé</p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
