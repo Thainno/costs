fa51c8f22eec92e49409dd10e8c80ff08f40d16f
Tainu <thainnosv@gmail.com>
2024-04-07 15:22:53 -0300

    project start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Empresa from "./pages/Empresa";
-import Contato from "./pages/Contato";
-import Home from "./pages/Home";
-import NavBar from "./components/layout/NavBar";
-import Footer from "./components/layout/Footer";
-
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route path="/empresa" element={<Empresa />}></Route>
-        <Route path="/contato" element={<Contato />}></Route>
-      </Routes>
-      <Footer />
-    </Router>
+    <div>
+      <p>Costs</p>
+    </div>
   );
 }
 
--- src/components/Saudacao.js
+++
@@ -1,9 +0,0 @@
-function Saudacao() {
-  return (
-    <>
-      <p>Saudação</p>
-    </>
-  );
-}
-
-export default Saudacao;
