39eb2c746f3c31a65f9ac52fb2b9f0b370a99ef0
Tainu <thainnosv@gmail.com>
2024-04-18 23:31:24 -0300

    create services card

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -10,10 +10,12 @@ import Container from "../layout/Container";
 import ProjectForm from "../projects/ProjectForm";
 import ServiceForm from "../service/ServiceForm";
 import Message from "../layout/Message";
+import ServiceCard from "../service/ServiceCard"
 
 function Project() {
   const { id } = useParams();
   const [project, setProject] = useState([]);
+  const [services, setServices] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
   const [showServiceForm, setShowServiceForm] = useState(false);
   const [message, setMessage] = useState();
@@ -30,6 +32,7 @@ function Project() {
         .then((resp) => resp.json())
         .then((data) => {
           setProject(data);
+          setServices(data.services);
         })
         .catch((err) => console.log(err));
     }, 300);
@@ -91,11 +94,12 @@ function Project() {
     })
       .then((resp) => resp.json())
       .then((data) => {
-        //exibir services
-        console.log(data);
+        setShowServiceForm(false)
       })
       .catch((err) => console.log(err));
   }
+
+  function removeService() {}
 
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
@@ -155,7 +159,18 @@ function Project() {
             </div>
             <h2>Serviços</h2>
             <Container customClass="start">
-              <p>Itens de serviços</p>
+              {services.length > 0 &&
+                services.map((service) => (
+                  <ServiceCard
+                    id={service.id}
+                    name={service.name}
+                    cost={service.cost}
+                    description={service.description}
+                    key={service.key}
+                    handleRemove={removeService}
+                  />
+                ))}
+              {services.length === 0 && <p>Não há serviços cadastrados.</p>}
             </Container>
           </Container>
         </div>
